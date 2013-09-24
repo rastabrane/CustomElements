c3de0dc2527eaa9375e2419fdc48bbb945cf55cf
Pete Blois <blois@google.com>
2013-09-24 11:05:33 -0700

    Merge fixes for running bootstrap when script has been loaded in interactive phase.
    BUG=
    R=jmesserly@google.com
    
    Review URL: https://codereview.appspot.com/13831050

--- src/boot.js
+++ src/boot.js
@@ -10,13 +10,13 @@ function bootstrap() {
   // parse document
   CustomElements.parser.parse(document);
   // one more pass before register is 'live'
-  CustomElements.upgradeDocument(document);  
+  CustomElements.upgradeDocument(document);
   // choose async
-  var async = window.Platform && Platform.endOfMicrotask ? 
+  var async = window.Platform && Platform.endOfMicrotask ?
     Platform.endOfMicrotask :
     setTimeout;
   async(function() {
-    // set internal 'ready' flag, now document.register will trigger 
+    // set internal 'ready' flag, now document.register will trigger
     // synchronous upgrades
     CustomElements.ready = true;
     // capture blunt profiling data
@@ -43,7 +43,8 @@ if (typeof window.CustomEvent !== 'function') {
 if (document.readyState === 'complete') {
   bootstrap();
 } else {
-  var loadEvent = window.HTMLImports ? 'HTMLImportsLoaded' : 'DOMContentLoaded';
+  var loadEvent = window.HTMLImports ? 'HTMLImportsLoaded' :
+      document.readyState == 'loading' ? 'DOMContentLoaded' : 'load';
   window.addEventListener(loadEvent, bootstrap);
 }
 
